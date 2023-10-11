172aebdc3747b8438bc2cf1114eef04f7f076333
AliNayab Nathani <alinayab@inspedium.com>
2023-10-11 12:18:27 +0500

    alignment

--- src/Components/VideoEdit/Card/SocialMediaCard.jsx
+++ src/Components/VideoEdit/Card/SocialMediaCard.jsx
@@ -9,17 +9,26 @@ import {
 } from '@chakra-ui/react';
 import React from 'react';
 import { AiOutlineClockCircle, BiMagicWand, FaRegCopy } from 'react-icons/all';
+import { ReactComponent as ThreadsIcon } from '../../../Assests/icon/ThreadsIcon.svg';
+import { ReactComponent as TwitterIcon } from '../../../Assests/icon/TwitterXLogo.svg';
 
 const SocialMediaCard = ({ data }) => {
   const { title, content } = data;
-  const formattedContent = content.replace(/\n/g, '&lt;br&gt;');
+  const formattedContent = content.replace(/\n/g, '<br>');
+  const icon =
+    title === 'Threads by Instagram' ? <ThreadsIcon /> : <TwitterIcon />;
+
   return (
     <Stack direction={['column', 'column']} mt={4}>
       <Stack bg={'white'} p={[4, 8]} borderRadius={'lg'}>
         <HStack justifyContent={'space-between'}>
-          <Text fontSize="20px" color="Black" fontWeight={'semibold'}>
-            {title}
-          </Text>
+          <HStack>
+            {' '}
+            {icon}
+            <Text fontSize="20px" color="Black" fontWeight={'semibold'}>
+              {title}
+            </Text>
+          </HStack>
 
           <HStack>
             <Button borderRadius={'full'} bg={'#FF5757'} color={'white'} ml={2}>
--- src/Components/VideoEdit/SocialComp.jsx
+++ src/Components/VideoEdit/SocialComp.jsx
@@ -9,20 +9,20 @@ const SocialComp = () => {
         content: `
           Post 1: 🌟 How did Mr. Beast achieve YouTube success? From no money to
           a dollar a day, he reinvested and worked hard for a decade. Dedication
-          pays off! 💪 #YoutubeSuccessStory\n\n
+          pays off! 💪 #YoutubeSuccessStory\n
 
           Post 2: 🌎 Did you know that less than 10% of the world speaks
           English? That means 90% is missing out on English content. Let's
-          embrace diversity and create content for everyone! 🌍 #ContentForAll\n\n
+          embrace diversity and create content for everyone! 🌍 #ContentForAll\n
 
           Post 3: 🚀 Spanish R1 is the biggest channel! Spanish-speaking content
           is thriving. Tap into the potential of other languages and cultures.
-          Expand your reach! 🌎 #LanguageIsKey\n\n
+          Expand your reach! 🌎 #LanguageIsKey\n
 
           Post 4: 🎥 Want to become a YouTuber? Learning from mistakes and
           improving is crucial. Join a community that fosters growth and shares
-          insights. Together, we can achieve success! 🤝 #CommunityOfCreators\n\n
-          
+          insights. Together, we can achieve success! 🤝 #CommunityOfCreators\n
+
           Post 5: 💔 School can be challenging for those with different
           passions. Don't let society dictate your destiny. Embrace your
           obsessions, work hard, and prove the skeptics wrong! 💯 #DreamBig
